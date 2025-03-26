934b872cf1e57450cb67f1e7042a0836e45ae41b
Ignaadex <100296517+Ignaadex@users.noreply.github.com>
2025-03-26 18:51:24 +0100

    Hss

--- mock-data.js
+++ mock-data.js
@@ -1,4 +1,4 @@
-export const mockData = {
+const mockData = {
   cards: [
     {
       id: 1000031,
@@ -16991,3 +16991,4 @@ export const mockData = {
     }
   ]
 }
+export { mockData }; // Export mockData
